51a84374c53cc3fc6513a66fe81e323821aaccb5
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2023-05-07 20:30:47 +0200

    feat: create AuthenticationException

---
+++ base/src/main/java/aiss/gitminer/exception/AuthenticationException.java
@@ -0,0 +1,17 @@
+package aiss.gitminer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
+import org.springframework.web.server.ResponseStatusException;
+
+public class AuthenticationException extends ResponseStatusException {
+
+    public AuthenticationException(HttpClientErrorException exception) {
+        this(exception instanceof Unauthorized ? HttpStatus.UNAUTHORIZED : HttpStatus.FORBIDDEN);
+    }
+
+    public AuthenticationException(HttpStatus status) {
+        super(status, "Service API responded with an " + status + " error.");
+    }
+}
