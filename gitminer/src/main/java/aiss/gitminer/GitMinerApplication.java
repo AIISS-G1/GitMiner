edc1e80398e1339ebaf9e8cfe610abfda4aa625e
mimbrero <mimbrero.alberto@gmail.com>
2023-05-10 17:42:33 +0200

    refactor: remove useless RequestMapping annotation

--- gitminer/src/main/java/aiss/gitminer/GitMinerApplication.java
+++ gitminer/src/main/java/aiss/gitminer/GitMinerApplication.java
@@ -2,10 +2,8 @@ package aiss.gitminer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping("/gitminer")
 public class GitMinerApplication {
 
 	public static void main(String[] args) {
