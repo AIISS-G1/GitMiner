c6b005f4efd3771d4fdb52a9c6fb783d33ae82b7
mimbrero <mimbrero.alberto@gmail.com>
2023-05-14 21:35:01 +0200

    test: create ProjectControllerTest

---
+++ gitminer/src/test/java/aiss/gitminer/controller/ProjectControllerTest.java
@@ -0,0 +1,136 @@
+package aiss.gitminer.controller;
+
+import aiss.gitminer.exception.EntityNotFoundException;
+import aiss.gitminer.model.Commit;
+import aiss.gitminer.model.Issue;
+import aiss.gitminer.model.Project;
+import aiss.gitminer.repository.ProjectRepository;
+import aiss.gitminer.service.CommitService;
+import aiss.gitminer.service.IssueService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.AdditionalMatchers.not;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProjectController.class)
+@Import(ProjectController.class)
+class ProjectControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+
+    @MockBean private ProjectRepository projectRepository;
+    @MockBean private CommitService commitService;
+    @MockBean private IssueService issueService;
+
+    private Project dummyProject;
+    private Commit dummyCommit;
+    private Issue dummyIssue;
+
+    @BeforeEach
+    void setUp() {
+        dummyProject = new Project();
+        dummyProject.setId(UUID.randomUUID().toString());
+
+        dummyCommit = new Commit();
+        dummyCommit.setId(UUID.randomUUID().toString());
+
+        dummyIssue = new Issue();
+        dummyIssue.setId(UUID.randomUUID().toString());
+    }
+
+    @Test
+    public void whenCallingFindAll_returnsProjects() throws Exception {
+        when(projectRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(List.of(dummyProject)));
+
+        MvcResult result = mockMvc.perform(get("/projects"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Project>>() {})).isEqualTo(List.of(dummyProject));
+        verify(projectRepository).findAll(any(Pageable.class));
+    }
+
+    @Test
+    public void givenValidId_whenCallingFindById_returnsProject() throws Exception {
+        when(projectRepository.findById("0")).thenReturn(Optional.of(dummyProject));
+
+        MvcResult result = mockMvc.perform(get("/projects/{id}", "0"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(objectMapper.readValue(result.getResponse().getContentAsString(), Project.class)).isEqualTo(dummyProject);
+    }
+
+    @Test
+    public void givenInvalidId_whenCallingFindById_throwsEntityNotFoundException() throws Exception {
+        when(projectRepository.findById(not(eq("0")))).thenThrow(new EntityNotFoundException());
+
+        mockMvc.perform(get("/projects/{id}", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenValidId_whenCallingFindCommits_returnsCommits() throws Exception {
+        when(projectRepository.existsById("0")).thenReturn(true);
+        when(commitService.findAll(eq("0"), isNull(), isNull(), isNull(), isNull(), isNull(), isNull(), any(Pageable.class)))
+                .thenReturn(new PageImpl<>(List.of(dummyCommit)));
+
+        MvcResult result = mockMvc.perform(get("/projects/{id}/commits", "0"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Commit>>() {}))
+                .isEqualTo(List.of(dummyCommit));
+    }
+
+    @Test
+    public void givenInvalidId_whenCallingFindCommits_throwsEntityNotFoundException() throws Exception {
+        when(projectRepository.existsById(not(eq("0")))).thenReturn(false);
+
+        mockMvc.perform(get("/issues/{id}/commits", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void givenValidId_whenCallingFindIssues_returnsIssues() throws Exception {
+        when(projectRepository.existsById("0")).thenReturn(true);
+        when(issueService.findAll(eq("0"), isNull(), isNull(), isNull(), isNull(), isNull(), isNull(), isNull(), isNull(), isNull(), any(Pageable.class)))
+                .thenReturn(new PageImpl<>(List.of(dummyIssue)));
+
+        MvcResult result = mockMvc.perform(get("/projects/{id}/issues", "0"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertThat(objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<Issue>>() {}))
+                .isEqualTo(List.of(dummyIssue));
+    }
+
+    @Test
+    public void givenInvalidId_whenCallingFindIssues_throwsEntityNotFoundException() throws Exception {
+        when(projectRepository.existsById(not(eq("0")))).thenReturn(false);
+
+        mockMvc.perform(get("/issues/{id}/issues", "1"))
+                .andExpect(status().isNotFound());
+    }
+}
